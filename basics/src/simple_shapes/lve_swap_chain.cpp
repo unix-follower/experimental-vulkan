bfa642b231a42d9a6980183a4daf00f8141a0c66
unix-follower <61003331+unix-follower@users.noreply.github.com>
2026-08-04 19:54:50 +0300

    Fix semaphore usage. (#3)
    One semaphore per frame-in-flight replaced with one semaphore per swap chain
    
    Co-authored-by: Artsem Nikitsenka <artsem_nikitsenka@epam.com>

--- basics/src/simple_shapes/lve_swap_chain.cpp
+++ basics/src/simple_shapes/lve_swap_chain.cpp
@@ -46,8 +46,10 @@ LveSwapChain::~LveSwapChain()
     vkDestroyRenderPass(device.device(), renderPass, nullptr);
 
     // cleanup synchronization objects
-    for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
+    for (size_t i = 0; i < imageCount(); i++) {
         vkDestroySemaphore(device.device(), renderFinishedSemaphores[i], nullptr);
+    }
+    for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
         vkDestroySemaphore(device.device(), imageAvailableSemaphores[i], nullptr);
         vkDestroyFence(device.device(), inFlightFences[i], nullptr);
     }
@@ -91,7 +93,7 @@ VkResult LveSwapChain::submitCommandBuffers(const VkCommandBuffer* buffers, uint
     submitInfo.commandBufferCount = 1;
     submitInfo.pCommandBuffers = buffers;
 
-    VkSemaphore signalSemaphores[] = {renderFinishedSemaphores[currentFrame]};
+    VkSemaphore signalSemaphores[] = {renderFinishedSemaphores[*imageIndex]};
     submitInfo.signalSemaphoreCount = 1;
     submitInfo.pSignalSemaphores = signalSemaphores;
 
@@ -339,7 +341,7 @@ void LveSwapChain::createDepthResources()
 void LveSwapChain::createSyncObjects()
 {
     imageAvailableSemaphores.resize(MAX_FRAMES_IN_FLIGHT);
-    renderFinishedSemaphores.resize(MAX_FRAMES_IN_FLIGHT);
+    renderFinishedSemaphores.resize(imageCount());
     inFlightFences.resize(MAX_FRAMES_IN_FLIGHT);
     imagesInFlight.resize(imageCount(), VK_NULL_HANDLE);
 
@@ -350,12 +352,17 @@ void LveSwapChain::createSyncObjects()
     fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
     fenceInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;
 
+    for (size_t i = 0; i < imageCount(); i++) {
+        if (vkCreateSemaphore(
+                device.device(), &semaphoreInfo, nullptr, &renderFinishedSemaphores[i]) !=
+            VK_SUCCESS) {
+            throw std::runtime_error(
+                "failed to create renderFinishedSemaphores objects for a frame!");
+        }
+    }
     for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
         if (vkCreateSemaphore(
                 device.device(), &semaphoreInfo, nullptr, &imageAvailableSemaphores[i]) !=
-                VK_SUCCESS ||
-            vkCreateSemaphore(
-                device.device(), &semaphoreInfo, nullptr, &renderFinishedSemaphores[i]) !=
                 VK_SUCCESS ||
             vkCreateFence(device.device(), &fenceInfo, nullptr, &inFlightFences[i]) != VK_SUCCESS) {
             throw std::runtime_error("Failed to create synchronization objects for a frame!");
