bfebe780da69e1d10b625432fd301de4cd730f64
unix-follower <61003331+unix-follower@users.noreply.github.com>
2026-08-05 20:27:28 +0300

    Handle window resize. (#6)
    Recreate swap chain. Use dynamic viewports
    
    Co-authored-by: Artsem Nikitsenka <artsem_nikitsenka@epam.com>

--- basics/src/simple_shapes/lve_device.hpp
+++ basics/src/simple_shapes/lve_device.hpp
@@ -37,7 +37,7 @@ class LveDevice {
 
     // Not copyable or movable
     LveDevice(const LveDevice&) = delete;
-    void operator=(const LveDevice&) = delete;
+    LveDevice& operator=(const LveDevice&) = delete;
     LveDevice(LveDevice&&) = delete;
     LveDevice& operator=(LveDevice&&) = delete;
 
--- basics/src/simple_shapes/lve_swap_chain.cpp
+++ basics/src/simple_shapes/lve_swap_chain.cpp
@@ -13,6 +13,20 @@ namespace simple_shapes {
 LveSwapChain::LveSwapChain(LveDevice& deviceRef, VkExtent2D extent)
     : device{deviceRef}, windowExtent{extent}
 {
+    init();
+}
+
+LveSwapChain::LveSwapChain(LveDevice& deviceRef,
+                           VkExtent2D extent,
+                           std::shared_ptr<LveSwapChain> previous)
+    : device{deviceRef}, windowExtent{extent}, oldSwapChain{previous}
+{
+    init();
+    oldSwapChain = nullptr;
+}
+
+void LveSwapChain::init()
+{
     createSwapChain();
     createImageViews();
     createRenderPass();
@@ -167,7 +181,7 @@ void LveSwapChain::createSwapChain()
     createInfo.presentMode = presentMode;
     createInfo.clipped = VK_TRUE;
 
-    createInfo.oldSwapchain = VK_NULL_HANDLE;
+    createInfo.oldSwapchain = oldSwapChain == nullptr ? VK_NULL_HANDLE : oldSwapChain->swapChain;
 
     if (vkCreateSwapchainKHR(device.device(), &createInfo, nullptr, &swapChain) != VK_SUCCESS) {
         throw std::runtime_error("failed to create swap chain!");
