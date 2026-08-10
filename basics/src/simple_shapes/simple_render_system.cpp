86df5b2a83c4b7c0fc677908c05257f931ebc6c8
Artsem Nikitsenka <artsem_nikitsenka@epam.com>
2026-08-10 20:21:17 +0300

    Add diffuse shading

--- basics/src/simple_shapes/simple_render_system.cpp
+++ basics/src/simple_shapes/simple_render_system.cpp
@@ -13,7 +13,7 @@ namespace simple_shapes {
 
 struct SimplePushConstantData {
     glm::mat4 transform{1.f};
-    alignas(16) glm::vec3 color{};
+    glm::mat4 normalMatrix{1.f};
 };
 
 SimpleRenderSystem::SimpleRenderSystem(LveDevice& device, VkRenderPass renderPass)
@@ -71,8 +71,9 @@ void SimpleRenderSystem::renderGameObjects(VkCommandBuffer commandBuffer,
 
     for (auto& obj : gameObjects) {
         SimplePushConstantData push{};
-        push.color = obj.color;
-        push.transform = projectionView * obj.transform.mat4();
+        auto modelMatrix = obj.transform.mat4();
+        push.transform = projectionView * modelMatrix;
+        push.normalMatrix = obj.transform.normalMatrix();
 
         vkCmdPushConstants(commandBuffer,
                            pipelineLayout,
