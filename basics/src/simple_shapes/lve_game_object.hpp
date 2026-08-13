aad74aa63e2e0df61c3489aed37b79b00f951198
Artsem Nikitsenka <artsem_nikitsenka@epam.com>
2026-08-13 11:14:14 +0300

    Add multiple point lights moving in circle

--- basics/src/simple_shapes/lve_game_object.hpp
+++ basics/src/simple_shapes/lve_game_object.hpp
@@ -19,7 +19,11 @@ struct TransformComponent {
     // https://en.wikipedia.org/wiki/Euler_angles#Rotation_matrix
     glm::mat4 mat4();
 
-    glm::mat3 normalMatrix();
+    glm::mat3 normalMatrix() const;
+};
+
+struct PointLightComponent {
+    float lightIntensity = 1.0f;
 };
 
 class LveGameObject {
@@ -33,6 +37,9 @@ class LveGameObject {
         return LveGameObject{currentId++};
     }
 
+    static LveGameObject
+    makePointLight(float intensity = 10.f, float radius = 0.1f, glm::vec3 color = glm::vec3(1.f));
+
     LveGameObject(const LveGameObject&) = delete;
     LveGameObject& operator=(const LveGameObject&) = delete;
     LveGameObject(LveGameObject&&) = default;
@@ -43,9 +50,12 @@ class LveGameObject {
         return id;
     }
 
-    std::shared_ptr<LveModel> model{};
     glm::vec3 color{};
     TransformComponent transform{};
+
+    // Optional pointer components
+    std::shared_ptr<LveModel> model{};
+    std::unique_ptr<PointLightComponent> pointLight = nullptr;
 
   private:
     LveGameObject(id_t objId) : id{objId} {}
